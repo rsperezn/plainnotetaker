b622cbc3f4b6ef9ae3ee108648a4164c00fcfd56
Santiago <rsperezn@gmail.com>
2016-05-04 22:00:13 +1000

    Added autosaved timestamp

--- app/src/main/java/com/rspn/plainnotetaker/NoteEditorActivity.java
+++ app/src/main/java/com/rspn/plainnotetaker/NoteEditorActivity.java
@@ -7,71 +7,81 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.rspn.plainnotetaker.data.NoteItem;
 import com.rspn.plainnotetaker.data.NotesDataSource;
 
+import java.util.Calendar;
+
 public class NoteEditorActivity extends Activity {
 
-	private NoteItem note;
-	private NotesDataSource dataSource;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_note_editor);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		dataSource = new NotesDataSource(this);
+    private NoteItem note;
+    private NotesDataSource dataSource;
+    private TextView edited_tv;
 
-		Intent intent = this.getIntent();
-		note = new NoteItem();
-		note.setKey(intent.getStringExtra("key"));
-		note.setText(intent.getStringExtra("text"));
-		
-		EditText editText = (EditText) findViewById(R.id.noteText);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        dataSource = new NotesDataSource(this);
+        edited_tv = (TextView) findViewById(R.id.textView_editedTime);
+        Intent intent = this.getIntent();
+        note = new NoteItem();
+        note.setKey(intent.getStringExtra("key"));
+        note.setText(intent.getStringExtra("text"));
 
-		editText.setText(note.getText());
-		editText.setSelection(note.getText().length());
-		editText.addTextChangedListener(new TextWatcher() {
-			@Override
-			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-			}
+        EditText editText = (EditText) findViewById(R.id.noteText);
 
-			@Override
-			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-				note.setText(charSequence.toString());
-				dataSource.update(note);
-			}
+        editText.setText(note.getText());
+        editText.setSelection(note.getText().length());
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
 
-			@Override
-			public void afterTextChanged(Editable editable) {
-			}
-		});
-	}
-	
-	private void saveAndFinish() {
-		EditText et = (EditText) findViewById(R.id.noteText);
-		String noteText = et.getText().toString();
-		
-		Intent intent = new Intent();
-		intent.putExtra("key", note.getKey());
-		intent.putExtra("text", noteText);
-		setResult(RESULT_OK, intent);
-		finish();
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                note.setText(charSequence.toString());
+                dataSource.update(note);
+            }
 
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == android.R.id.home) {
-			saveAndFinish();
-		}
-		return false;
-	}
-	
-	@Override
-	public void onBackPressed() {
-		saveAndFinish();
-	}
-	
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Calendar calendar = Calendar.getInstance();
+                edited_tv.setText(getString(
+                        R.string.autosaved,
+                        calendar.get(Calendar.HOUR),
+                        calendar.get(Calendar.MINUTE),
+                        calendar.get(Calendar.AM_PM) == 1 ? "PM" : "AM"));
+            }
+        });
+    }
+
+    private void saveAndFinish() {
+        EditText et = (EditText) findViewById(R.id.noteText);
+        String noteText = et.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra("key", note.getKey());
+        intent.putExtra("text", noteText);
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            saveAndFinish();
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        saveAndFinish();
+    }
+
 }
