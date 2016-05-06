c15fe6bf23aafa7d6c3d02927e2f994f7f3ca9de
Santiago <rsperezn@gmail.com>
2016-05-06 18:52:51 +1000

    Removed button to create note from actionBar

--- app/src/main/java/com/rspn/plainnotetaker/MainActivity.java
+++ app/src/main/java/com/rspn/plainnotetaker/MainActivity.java
@@ -63,13 +63,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
     private void createNote() {
         NoteItem note = NoteItem.getNew();
         Intent intent = new Intent(this, NoteEditorActivity.class);
